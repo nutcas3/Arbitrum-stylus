b9ab180d8e6ec8982c072e5cee37108c61f25a3b
nutcas3 <mauricee423@gmail.com>
2024-08-17 00:20:45 +0300

    hello stylus

---
+++ hello_stylus/src/lib.rs
@@ -0,0 +1,18 @@
+#![no_main]
+#![no_std]
+extern crate alloc;
+
+#[global_allocator]
+static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
+
+use alloc::vec::Vec;
+
+use stylus_sdk::{console, stylus_proc::entrypoint};
+
+#[entrypoint]
+fn user_main(input: Vec<u8>) -> Result<Vec<u8>, Vec<u8>> {
+
+    console!("Hello Stylus!");
+    Ok(input)
+}
+
