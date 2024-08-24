05d8b3f13a185a3ce667a6f76546366158e175ee
nutcas3 <mauricee423@gmail.com>
2024-08-24 13:27:57 +0300

    tests and cargo

--- erc20/src/erc20.rs
+++ erc20/src/erc20.rs
@@ -1,13 +1,3 @@
-//! Implementation of the ERC-20 standard
-//!
-//! The eponymous [`Erc20`] type provides all the standard methods,
-//! and is intended to be inherited by other contract types.
-//!
-//! You can configure the behavior of [`Erc20`] via the [`Erc20Params`] trait,
-//! which allows specifying the name, symbol, and decimals of the token.
-//!
-//! Note that this code is unaudited and not fit for production use.
-
 // Imported packages
 use alloc::string::String;
 use alloy_primitives::{Address, U256};
